01240e954a761388dea2f5057efb354c289b372d
Shyla Clark <sclark@geocent.com@GEOLAP1898-476235.local>
2020-02-28 15:16:16 -0600

    Added a max_dams custom setting

--- tethysdev/tethysapp-dam_inventory/tethysapp/dam_inventory/app.py
+++ tethysdev/tethysapp-dam_inventory/tethysapp/dam_inventory/app.py
@@ -1,5 +1,5 @@
 from tethys_sdk.base import TethysAppBase, url_map_maker
-
+from tethys_sdk.app_settings import CustomSetting
 
 class DamInventory(TethysAppBase):
     """
@@ -11,11 +11,25 @@ class DamInventory(TethysAppBase):
     icon = 'dam_inventory/images/icon.gif'
     package = 'dam_inventory'
     root_url = 'dam-inventory'
-    color = '#165751'
+    color = '#15325e'
     description = 'A Tethys app created while doing the tutorial from docs.tethysplatform.org'
     tags = ''
     enable_feedback = False
     feedback_emails = []
+
+    def custom_settings(self):
+        """
+        Example custom settings method.
+        """
+        custom_settings = (
+            CustomSetting(
+                name='max_dams',
+                type=CustomSetting.TYPE_INTEGER,
+                description='Maximum number of dams that can be created in the app.',
+                required=False
+            ),
+        )
+        return custom_settings
 
     def url_maps(self):
         """
