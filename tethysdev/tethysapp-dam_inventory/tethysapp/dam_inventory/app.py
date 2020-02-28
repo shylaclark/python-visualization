8d603df8a207cf8f5439ac3e82f82f473f37d4a1
Shyla Clark <sclark@geocent.com@GEOLAP1898-476235.local>
2020-02-28 15:04:06 -0600

    Created a new Tethys app based on the Dam Inventory tutorial

---
+++ tethysdev/tethysapp-dam_inventory/tethysapp/dam_inventory/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class DamInventory(TethysAppBase):
+    """
+    Tethys app class for Dam Inventory.
+    """
+
+    name = 'Dam Inventory'
+    index = 'dam_inventory:home'
+    icon = 'dam_inventory/images/icon.gif'
+    package = 'dam_inventory'
+    root_url = 'dam-inventory'
+    color = '#165751'
+    description = 'A Tethys app created while doing the tutorial from docs.tethysplatform.org'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='dam-inventory',
+                controller='dam_inventory.controllers.home'
+            ),
+        )
+
+        return url_maps
