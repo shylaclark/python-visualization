a7efe0cd576671779a5f9fb12c0f482054be2a9a
Shyla Clark <sclark@geocent.com@GEOLAP1898-478823.local>
2020-03-02 10:52:47 -0600

    Updated template with add dam form and updated controller with dropdowns, initial values, error messages

--- tethysdev/tethysapp-dam_inventory/tethysapp/dam_inventory/controllers.py
+++ tethysdev/tethysapp-dam_inventory/tethysapp/dam_inventory/controllers.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
 from tethys_sdk.permissions import login_required
-from tethys_sdk.gizmos import MapView, Button
+from tethys_sdk.gizmos import MapView, Button, TextInput, DatePicker, SelectInput
 
 @login_required()
 def home(request):
@@ -95,11 +96,94 @@ def add_dam(request):
     """
     Controller for the Add Dam page.
     """
+
+    # Default values
+    name = ''
+    owner = 'Reclamation'
+    river = ''
+    date_built = ''
+
+    # Errors
+    name_error = ''
+    owner_error = ''
+    river_error = ''
+    date_error = ''
+
+    # Handle form submission
+    if request.POST and 'add-button' in request.POST:
+        # Get values
+        has_errors = False
+        name = request.POST.get('name', None)
+        owner = request.POST.get('owner', None)
+        river = request.POST.get('river', None)
+        date_built = request.POST.get('date-built', None)
+
+        # Validate
+        if not name:
+            has_errors = True
+            name_error = 'Name is required.'
+
+        if not owner:
+            has_errors = True
+            owner_error = 'Owner is required.'
+
+        if not river:
+            has_errors = True
+            river_error = 'River is required.'
+
+        if not date_built:
+            has_errors = True
+            date_error = 'Date Built is required.'
+
+        if not has_errors:
+            # Do stuff here
+            return redirect(reverse('dam_inventory:home'))
+
+        messages.error(request, "Please fix errors.")
+
+    # Define form gizmos
+    name_input = TextInput(
+        display_text='Name',
+        name='name',
+        initial=name,
+        error=name_error
+    )
+
+    owner_input = SelectInput(
+        display_text='Owner',
+        name='owner',
+        multiple=False,
+        options=[('Reclamation', 'Reclamation'), ('Army Corp', 'Army Corp'), ('Other', 'Other')],
+        initial=owner,
+        error=owner_error
+    )
+
+    river_input = TextInput(
+        display_text='River',
+        name='river',
+        placeholder='e.g.: Mississippi River',
+        initial=river,
+        error=river_error
+    )
+
+    date_built = DatePicker(
+        display_text='Date Built',
+        name='date-built',
+        autoclose=True,
+        format='MM d, yyyy',
+        start_view='decade',
+        today_button=True,
+        initial=date_built,
+        error=date_error
+    )
+
     add_button = Button(
         display_text='Add',
         name='add-button',
         icon='glyphicon glyphicon-plus',
-        style='success'
+        style='success',
+        attributes={'form': 'add-dam-form'},
+        submit=True
     )
 
     cancel_button = Button(
@@ -109,6 +193,10 @@ def add_dam(request):
     )
 
     context = {
+        'name_input': name_input,
+        'owner_input': owner_input,
+        'river_input': river_input,
+        'date_built_input': date_built,
         'add_button': add_button,
         'cancel_button': cancel_button,
     }
